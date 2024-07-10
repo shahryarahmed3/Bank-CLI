52e4dbb0d2bdd2791224423d27929013fbdef376
Shahryar A. <96958885+shahryarahmed3@users.noreply.github.com>
2024-07-10 19:14:26 +0000

    updated bank

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-# ruby-cli-template
+# Bank-CLI
 
 Use this repository as a template for your Ruby CLI (command line interface) project.
 
 - Ruby version: `3.2.1`
+
+This Bank command line interface app, allows instance(s) of a bank to be created, and 
+be populated with instances of bank accounts. 
+
+
+Bank - A financial institution object, with attributes: 
+  - bank_name (String which stores the name of the financial institution)
+  - num_accts (Integer which stores the number of accts at a single institute,
+              useful for generating unique new bank account numbers)
+  - accounts (Hash data structure which stores => account : bank account object)
+  
+Bank Account - A user financial services account object, with attributes:
+  - account_holder (String which stores the full name of the account holder)
+  - account_number (Integer which stores an account number)
+  - balance (Integer which stores the balance information)
+  - pin (Integer which stores a 4-digit pin for user authentication)
+
+
+[Menu Screen] - User has option to navigate the different services offered at "ABC Bank".
+                This menu reappears after each transaction is complete allowing the user
+                to make additional transactions.
+
+    |||||||||||||||||||||| [ Welcome to ABC Bank! ] ||||||||||||||||||||||||
+
+    ||||||||||||||||||||||||| [ Transaction Menu ] |||||||||||||||||||||||||
+
+
+      1. Open a new account
+      2. Deposit
+      3. Withdraw
+      4. Balance
+      5. Exit
+
+    Please select an option ==> 
--- bankaccount.rb
+++ bankaccount.rb
@@ -15,20 +15,24 @@ class Bank
   def register_new_acct(account_number, bank_acct)
     @accounts[account_number] = bank_acct
   end
-  def find_acct (account_number)
+  def find_acct (account_number, pin)
     acct = self.get_acct(account_number)
+    if (pin != acct.pin)
+      puts "  Invalid pin."
+      return nil
+    end
     return acct
   end
 end
 
 class BankAccount < Bank
-  attr_accessor :account_holder, :account_number, :routing_number, :balance
-  @num_accts = 100
+  attr_accessor :account_holder, :account_number, :balance, :pin
 
-  def initialize (account_holder, initial_balance, bank)
+  def initialize (account_holder, initial_balance, bank, pin)
     @account_holder = account_holder
     @account_number = bank.get_acct_num
     @balance = initial_balance
+    @pin = pin
   end
 
   def get_account_holder_name
@@ -50,14 +54,23 @@ class BankAccount < Bank
   def withdraw (amount)
     self.balance = self.balance - amount;
   end
+
+  def get_pin
+    return self.pin
+  end
+
+  def authenticate_user (pin)
+    return self.pin == pin
+  end
 end
 
-print "\n\nWelcome to ABC Bank!"
+puts "\n\n|||||||||||||||||||||| [ Welcome to ABC Bank! ] ||||||||||||||||||||||||"
 
 bank1 = Bank.new
 
 
 while true
+  puts "\n||||||||||||||||||||||||| [ Transaction Menu ] |||||||||||||||||||||||||"
   puts "\n\n  1. Open a new account"
   puts "  2. Deposit"
   puts "  3. Withdraw"
@@ -69,17 +82,19 @@ while true
 
   case choice
     when "1", "new_acct"
-      puts "\n====================== ABC Bank ========================"
+      puts "\n||||||||||||||||||||||||||| [ New Account ] ||||||||||||||||||||||||||||"
       puts "\nTo open a new bank account, please provide the following..."
       print "\nFirst Name: "
-        first_name = gets.chomp.downcase
+        first_name = gets.chomp.capitalize
       print "Last Name: "
-        last_name = gets.chomp.downcase
+        last_name = gets.chomp.capitalize
+      print "Create a new 4-digit PIN: "
+        pin = gets.chomp.to_i
       print "How much do you want to deposit? "
         initial_dep = gets.chomp.to_i
 
       name = "#{first_name} #{last_name}"
-      my_acct = BankAccount.new(name, initial_dep, bank1)
+      my_acct = BankAccount.new(name, initial_dep, bank1, pin)
       bank1.register_new_acct(my_acct.get_account_number, my_acct)
  
     
@@ -87,61 +102,80 @@ while true
       puts "\n  Account Holder: #{my_acct.get_account_holder_name}"
       puts "  Account Number: #{my_acct.get_account_number}"
       puts "  Initial Deposit: $#{my_acct.get_balance}"
-      puts "\n================== End of Transaction ===================="
+      puts "  Your 4-digit PIN: #{my_acct.get_pin}"
+      puts "\n   ================== -[ End of Transaction ]- ===================="
 
     when "2", "deposit"
-      puts "\n====================== ABC Bank ========================"
+      puts "\n||||||||||||||||||||||||||| [ Deposit ] |||||||||||||||||||||||||||||||"
       puts "\nTo make a deposit, please provide the following..."
       print "\n  Account Number: "
         account_num = gets.chomp.to_i
       print "\n  How much would you like to deposit? "
         deposit_amt = gets.chomp.to_i
+      print "\n  Enter your 4-digit Pin: "
+        pin = gets.chomp.to_i
 
-      my_acct = bank1.find_acct(account_num)
-      my_acct.deposit(deposit_amt)
-
-      puts "\nHere's a summary of your deposit transaction..."
-      puts "\n  Account Holder: #{my_acct.get_account_holder_name}"
-      puts "  Account Number: #{my_acct.get_account_number}"
-      puts "  Deposit Amount: #{deposit_amt}"
-      puts "  Account Balance: $#{my_acct.get_balance}"
-      puts "\n================== End of Transaction ===================="
+      my_acct = bank1.find_acct(account_num, pin)
+      if my_acct != nil
+        my_acct.deposit(deposit_amt)
+        puts "\nHere's a summary of your deposit transaction..."
+        puts "\n  Account Holder: #{my_acct.get_account_holder_name}"
+        puts "  Account Number: #{my_acct.get_account_number}"
+        puts "  Deposit Amount: #{deposit_amt}"
+        puts "  Account Balance: $#{my_acct.get_balance}"
+        puts "\n   ================== -[ End of Transaction ]- ===================="
+      else
+        puts "\n  Account not found, please try again."
+        puts "\n   ================== -[ End of Transaction ]- ===================="
+      end
 
     when "3", "withdraw"
-      puts "\n====================== ABC Bank ========================"
+      puts "\n||||||||||||||||||||||||||| [ Withdraw ] |||||||||||||||||||||||||||||||"
+
       puts "\nTo withdraw money, please provide the following..."
       print "\n  Account Number: "
         account_num = gets.chomp.to_i
       print "\n  How much would you like to withdraw? "
         withdraw_amt = gets.chomp.to_i
-      print "\n  4-digit Pin: "
+      print "\n  Enter your 4-digit Pin: "
         pin = gets.chomp.to_i
 
-      my_acct = bank1.find_acct(account_num)
-      my_acct.withdraw(withdraw_amt)
+      my_acct = bank1.find_acct(account_num, pin)
 
-      puts "\nHere's a summary of your withdrawl transaction..."
-      puts "\n  Account Holder: #{my_acct.get_account_holder_name}"
-      puts "  Account Number: #{my_acct.get_account_number}"
-      puts "  Withdraw Amount: #{withdraw_amt}"
-      puts "  Account Balance: $#{my_acct.get_balance}"
-      puts "\n================== End of Transaction ===================="
-      
+      if my_acct != nil
+        my_acct.withdraw(withdraw_amt)
+        puts "\nHere's a summary of your withdrawl transaction..."
+        puts "\n  Account Holder: #{my_acct.get_account_holder_name}"
+        puts "  Account Number: #{my_acct.get_account_number}"
+        puts "  Withdraw Amount: #{withdraw_amt}"
+        puts "  Account Balance: $#{my_acct.get_balance}"
+        puts "\n   ================== -[ End of Transaction ]- ===================="
+      else
+        puts "  Account not found, please try again."
+        puts "\n   ================== -[ End of Transaction ]- ===================="
+      end
+
     when "4", "balance"
-      puts "\n====================== ABC Bank ========================"
+      puts "\n||||||||||||||||||||||||||| [ Balance ] |||||||||||||||||||||||||||||||"
+
       puts "\nFor the account balance, please provide the following..."
       print "\n  Account Number: "
         account_num = gets.chomp.to_i
-      print "\n  4-digit Pin: "
+      print "\n  Enter your 4-digit Pin: "
         pin = gets.chomp.to_i
-      
-      my_acct = bank1.find_acct(account_num)
 
-      puts "\nYour account balance is..."
-      puts "\n  Account Holder: #{my_acct.get_account_holder_name}"
-      puts "  Account Number: #{my_acct.get_account_number}"
-      puts "  Account Balance: $#{my_acct.get_balance}"
-      puts "\n================== End of Transaction ===================="
+        my_acct = bank1.find_acct(account_num, pin)
+
+      if my_acct != nil
+        puts "\nYour account balance is..."
+        puts "\n  Account Holder: #{my_acct.get_account_holder_name}"
+        puts "  Account Number: #{my_acct.get_account_number}"
+        puts "  Account Balance: $#{my_acct.get_balance}"
+        puts "\n   ================== -[ End of Transaction ]- ===================="
+      else
+        puts "  Account not found, please try again."
+        puts "\n   ================== -[ End of Transaction ]- ===================="
+      end
     when "5", "exit"
       puts "Thank you for choosing ABC Bank"
       break
